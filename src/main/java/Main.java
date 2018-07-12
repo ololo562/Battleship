1470a56015f2e65d9253db28528ec371b640aaf3
Martyn562 <Kaminetskyi.Martyn@gmail.com>
2018-07-12 17:11:04 +0300

    fix project structure added git ignore

---
+++ src/main/java/Main.java
@@ -0,0 +1,45 @@
+import javafx.scene.layout.Background;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame f=new JFrame();//creating instance of JFrame
+
+        for(int i=0; i<10;i++){
+            for(int j=0; j<10;j++){
+
+//enemy starts
+                JButton enemy  = new JButton("");//creating instance of JButton
+                enemy.setBounds(280+16*i,20+16*j,15, 15);
+                f.add(enemy);
+
+                enemy.addActionListener((e)-> {
+                    if(enemy.getBackground()!=(Color.BLUE))
+                    enemy.setBackground(Color.GREEN);
+                });
+
+//my starts
+                JButton my  = new JButton("");//creating instance of JButton
+                my.setBounds(20+16*i,20+16*j,15, 15);
+                f.add(my);
+
+                my.addActionListener((e)-> {
+                    my.setBackground(Color.BLUE);
+                    if(enemy.getBackground()==(Color.GREEN))
+                        enemy.setBackground(Color.RED);
+                    else
+                        enemy.setBackground(Color.BLUE);
+                });
+
+
+            }
+        }
+
+        //  f.add(b);//adding button in JFrame
+        f.setSize(480,240);
+        f.setLayout(null);//using no layout managers
+        f.setVisible(true);//making the frame visible
+    }
+}  
